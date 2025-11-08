2f55361be9124d1b417579d7ba485246381de9bb
Denen <awardenen@gmail.com>
2025-11-08 08:23:18 +0000

    Created and Edited python_introduction/basic_operation.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#Assigning values to variables
+number1 = 50
+number2 = 10
+
+#Basic arithmetic operations
+add = number1 + number2
+sub = number1 - number2
+mut = number1 * number2
+
+# print results
+print(f'Addition of {number1} and {number2} is {add}')
+print(f'Subtraction of {number1} and {number2} is {sub}')
+print(f'Multiplication of {number1} and {number2} is {mut}')
