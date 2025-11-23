9b876d0f6a38adeaec290e112af5617f816f5058
Denen <awardenen@gmail.com>
2025-11-23 21:36:13 +0100

    Added a module named arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            result = num1 + num2
+        case 'subtract':
+            result = num1 - num2
+        case 'multiply':
+            result = num1 * num2
+        case 'divide' if num2 != 0:
+            result = num1 / num2
+        case 'divide' if num2 == 0:
+            result = 'Error: Cannot divide by zero'
+        case _:
+            result = 'Error: Invalid operation'
+    return result
