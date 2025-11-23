877490c9f454fd4ff1309dcbb21f8c3890d0c03f
Denen <awardenen@gmail.com>
2025-11-23 21:42:36 +0100

    Updated arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,10 +7,10 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             result = num1 * num2
         case 'divide':
-            if num2 != 0:
-                result = num1 / num2
-            else:
+            if num2 == 0:
                 result = 'Error: Division by zero'
+            else:
+                result = num1 / num2
         case _:
             result = 'Error: Invalid operation'
     return result
