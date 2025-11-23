24f76b07f0ad85ff7dbd00e4369dcc591d973f4a
Denen <awardenen@gmail.com>
2025-11-23 21:44:01 +0100

    Added elif condition

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 result = 'Error: Division by zero'
-            else:
+            elif num2 != 0:
                 result = num1 / num2
         case _:
             result = 'Error: Invalid operation'
