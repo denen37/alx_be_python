78ea65acff2afa589f1aa40261185c2b434599ae
Denen <awardenen@gmail.com>
2025-11-23 22:46:51 +0100

    Correct the formatting

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,7 +9,7 @@ def calculate_future_date():
     days_ahead  = int(input("Enter the number of days to add to the current date: "))
     current_date = datetime.now()
     future_date = current_date + timedelta(days=days_ahead)
-    print(f"Future date: {datetime.strftime(future_date, '%Y-%m-%d')}")
+    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
 
 
 
